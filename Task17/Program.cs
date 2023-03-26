5eb146afa7b3093254210853b1e1623997da2b1e
Igor Kazakov <kiw@lefterova.ru>
2023-03-26 15:51:14 +0500

    first commit

---
+++ Task17/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 17
+// Напишите программу, которая принимает на вход координаты точки (x, y)
+// причем x != 0, y != 0 и выдает номер четверти плоскости, 
+// в которой находится эта точка
+
+int[] points = new int[2]; // создаем массив, обозначая его с помощью [], длина этого массива равна 2 (координаты на плоскости)
+
+Console.WriteLine("Input X & Y");
+for (int i = 0; i < points.Length; i ++)// вместо цикла while можно использовать цикл for
+    points[i] = Convert.ToInt32(Console.ReadLine());
+if(points[0] > 0 && points[1] > 0) //points[0] - X, points[1] - Y
+    Console.WriteLine("It`s first quadrant");
+    else if(points[0] < 0 && points[1] > 0) 
+        Console.WriteLine("It`s second quadrant");
+        else if(points[0] < 0 && points[1] < 0) 
+            Console.WriteLine("It`s third quadrant");
+            else if(points[0] > 0 && points[1] < 0) 
+                Console.WriteLine("It`s fourth quadrant");
+                else Console.WriteLine("координаты X или Y = 0");
---
+++ Task21/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 2D пространстве
+// Примеры:
+// A(3, 6); B(2, 1) -> 5,09
+// A(7, -5); B(1, -1) -> 7,21
+
+double[] pointA = new double [2];
+double[] pointB = new double [2];
+double distance = 0;
+
+Console.WriteLine("введите координаты первой точки - X и Y");
+for (int i = 0; i < pointA.Length; i ++)
+    pointA[i] = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("введите координаты второй точки - X и Y");
+for (int i = 0; i < pointB.Length; i ++)
+    pointB[i] = Convert.ToDouble(Console.ReadLine());
+
+distance = Math.Sqrt(Math.Pow(pointA[0] - pointB[0], 2) + Math.Pow(pointA[1] - pointB[1], 2));
+Console.WriteLine($"Растояние между точками равно {Math.Round(distance, 3)}");
